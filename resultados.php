0f37213fc66bd2ddb570867df3e8aa8758a4204f
filipijorge <109801332+filipijorge@users.noreply.github.com>
2023-10-10 14:45:04 -0300

    Update resultados.php

--- resultados.php
+++ resultados.php
@@ -1,8 +1,7 @@
 <html lang="en">
     <head>
         <meta charset="utf-8">
-        <meta name="viewport" content="width=device-width, initial-scale=1">
-        <title>Cadastro</title>
+        <title>Resultados</title>
         <link href="css/bootstrap.min.css" rel="stylesheet">
         <link href="css/custom.css" rel="stylesheet">
     </head>
