0f63fa411f31cad3968deca14cfa0f4266ebb807
filipijorge <109801332+filipijorge@users.noreply.github.com>
2023-11-06 10:43:21 -0300

    Update index.php

--- index.php
+++ index.php
@@ -1,5 +1,52 @@
 <?php
-if ($_SERVER['REMOTE_ADDR'] == '192.168.0.1') { ?>
+if ($_SERVER['REMOTE_ADDR'] == '192.168.0.1') {
+    vote();
+} elseif (isset($_SERVER['PHP_AUTH_USER']) && $_SERVER['PHP_AUTH_USER'] == "usuario" && $_SERVER['PHP_AUTH_PW'] == "senha") {
+    vote(); // Permite que usuários autenticados acessem a função vote
+} else {
+    if (isset($_SERVER['PHP_AUTH_USER'])) {
+        // Usuário forneceu credenciais, mas elas estão erradas
+        authenticationFailed();
+    } else {
+        // Usuário não forneceu credenciais, exiba prompt de autenticação
+        authenticate();
+    }
+}
+
+function authenticationFailed() {
+    header("HTTP/1.0 401 Unauthorized"); ?>
+    <html lang="pt-br">
+        <head>
+            <meta charset="utf-8">
+            <title>Acesso Negado</title>
+            <link href="css/bootstrap.min.css" rel="stylesheet">
+            <link href="css/custom.css" rel="stylesheet">
+        </head>
+        <body>
+            <div class="container text-center d-flex align-items-center justify-content-center">
+                <div class="row">			
+                    <div class="col mt-5">
+                        <h1>Acesso Negado</h1>
+                        <p>Usuário ou senha incorretos. Tente novamente.</p>
+                        <form method="post">
+                            <button type="submit"  name="retryAuthentication" class="btn btn-warning">Tentar Novamente</button>
+                        </form>
+                    </div>
+                </div>
+            </div>
+        </body>
+    </html><?php
+    exit;
+}
+
+function authenticate() {
+    header("WWW-Authenticate: Basic realm='Avaliacao'");
+    header("HTTP/1.0 401 Unauthorized");
+    canceled();
+    exit;
+}
+
+function vote() { ?>   
     <html lang="pt-br">
         <head>
             <meta charset="utf-8">
@@ -61,9 +108,10 @@ if ($_SERVER['REMOTE_ADDR'] == '192.168.0.1') { ?>
             </div>
             <script src="js/bootstrap.bundle.min.js"></script>
         </body>
-    </html>
-    <?php
-} else { ?>
+    </html><?php 
+}
+
+function canceled() { ?>
     <html lang="pt-br">
         <head>
             <meta charset="utf-8">
@@ -76,7 +124,10 @@ if ($_SERVER['REMOTE_ADDR'] == '192.168.0.1') { ?>
                 <div class="row">			
                     <div class="col mt-5">
                         <h1>Acesso Negado</h1>
-                        <p>Contate o administrador do sistema.</p>
+                        <p>Faça login para continuar.</p>
+                        <form method="post">
+                            <button type="submit"  name="retryAuthentication" class="btn btn-warning">Tentar Novamente</button>
+                        </form>
                     </div>
                 </div>
             </div>
